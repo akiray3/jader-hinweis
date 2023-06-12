a1d9277e89b809a73fd7728a8f3bf8c3828c3dc3
akiray3 <ayoshida1017@gmail.com>
2023-06-12 17:00:22 +0900

    adding DT

--- app.R
+++ app.R
@@ -1,11 +1,6 @@
-library(shiny)
-library(shinydashboard)
-library(ggplot2)
-library(tidyverse)
-library(DT)
-library(ggrepel)
-library(openxlsx)
-library(plotly)
+library("needs")
+needs(c(shiny, shinydashboard, ggplot2, tidyverse, DT, ggrepel, openxlsx, plotly))
+
 
 shiny::shinyApp(
   ui = dashboardPage(
@@ -14,53 +9,60 @@ shiny::shinyApp(
     dashboardSidebar(
       collapsed = TRUE,
       sidebarMenu(
-        menuItem(
-          text = "Page1",
-          tabName = "page1",
-          icon = icon("chart-line")
-        ),
-        menuItem(
-          text = "Help",
-          tabName = "help",
-          icon = icon("house")
-        )
+        menuItem(text = "Page1", tabName = "page1", icon = icon("chart-line")),
+        menuItem(text = "Help", tabName = "help", icon = icon("house"))
       )
     ),
     dashboardBody(
       tabItems(
         tabItem(
           tabName = "page1",
-          tags$link(
-            rel = "stylesheet",
-            type = "text/css",
-            href = "style.css"
-          ),
+          tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
           fluidRow(
             box(
               width = 12,
               solidHeader = TRUE,
               status = "primary",
-              title = "有害事象マップから症状を選択",
-              plotly::plotlyOutput("plot01am"),
+              title = "有害事象マップ",
+              DTOutput("mydatatable"),
               div(class = "space"),
-              uiOutput("clpointinfo01am"),
-              uiOutput("cldownload01am"),
-              uiOutput("cldbt01am")
+              uiOutput("cldownload01"),
+              uiOutput("cldbt01")
             )
           )
         ),
         tabItem(
           tabName = "help",
-          tags$link(
-            rel = "stylesheet",
-            type = "text/css",
-            href = "pagestyle.css"
-          ),
+          tags$link(rel = "stylesheet", type = "text/css", href = "pagestyle.css"),
           htmlTemplate("page.html")
         )
       )
     )
   ),
-  server = function(input, output) {
+  server = function(input, output) {  
+    tbl200 <- readRDS("data/tbl_200.obj")
+    tblall <- readRDS("data/tbl_all.obj")
+    # tbl200 <- reactive({readRDS("data/tbl_200.obj")})
+    # tblall <- reactive({readRDS("data/tbl_all.obj")})
+    output$mydatatable <- renderDT({
+        tbl200 %>%
+          dplyr::select(性別, 順位, クラス, 有害事象, 件数, 高齢発症ROR, 多剤発症ROR) %>%
+          dplyr::mutate(
+            高齢発症ROR = round(高齢発症ROR, 2),
+            多剤発症ROR = round(多剤発症ROR, 2)
+          ) %>%
+          DT::datatable(
+            class = "cell-border stripe",
+            style = 'bootstrap',
+            selection = "single",
+            filter = "top",
+            option = list(
+              autoFill = TRUE, responsive = TRUE, scrollX = TRUE, autoWidth = TRUE,
+              searchBuilder = list(enterSearch = TRUE),
+              searchPanes = list(il8n = list(loadMessage = "loading...")),
+              select = list(style = "multi", selector = "td:first-child")
+            )
+          )
+      })
   }
 )
